472c1af6952da53a1600ed57b059379cd6faa727
Bjorn Carell <bjorn.carell@tink.com>
2021-08-09 19:56:00 +0200

    feat(project): init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Learning TypeScript.</h3>
+      <p>Tic Tac Toe</p>
+    </div>
+  );
+}
+
+export default App;
